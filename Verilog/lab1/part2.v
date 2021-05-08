b31fdf9e118d075336aa0057df4bbd21b27700a9
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-05-08 11:17:57 +0530

    Update part2.v

--- Verilog/lab1/part2.v
+++ Verilog/lab1/part2.v
@@ -1,5 +1,5 @@
 module part2 (SW, LEDG);
-	input [17:0]SW;
-	output [7:0]LEDG;
-	assign LEDG[7:0] = (~SW[17] & SW[7:0]) | (SW[17] & SW[15:8]);
+	input [9:0]SW;
+	output [3:0]LEDG;
+	assign LEDG[3:0] = (~SW[9] & SW[3:0]) | (SW[9] & SW[7:4]);
 endmodule
