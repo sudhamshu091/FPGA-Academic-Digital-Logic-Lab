1b7293fc86b1c34ae490ba2a5ef9e17ee9fc6c2f
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-05-09 13:50:18 +0530

    Update part1.v

--- Verilog/lab3/part1.v
+++ Verilog/lab3/part1.v
@@ -2,7 +2,7 @@ module part1 (Clk, R, S, Q);
   input Clk, R, S;
   output Q;
 
-  wire R_g, S_g, Qa, Qb;
+  wire R_g, S_g, Qa, Qb /* synthesis keep */;
 
   assign R_g = R & Clk;
   assign S_g = S & Clk;
