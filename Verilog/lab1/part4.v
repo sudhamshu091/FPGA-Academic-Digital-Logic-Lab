8b7afbc81c96c46d4ececa8b9fd95ad4aab9b6c8
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-06-02 20:15:24 +0530

    Update part4.v

--- Verilog/lab1/part4.v
+++ Verilog/lab1/part4.v
@@ -2,11 +2,11 @@ module part4 (SW, HEX0);
     input [1:0] SW;
     output reg [6:0] HEX0;
 
-    always @(SW)
-        case (SW[1:0])
-            2'b00: HEX0 = 7'b0100001;
-            2'b01: HEX0 = 7'b0000110;
-            2'b10: HEX0 = 7'b1111001;
-            2'b11: HEX0 = 7'b1000000;
-        endcase
+    assign HEX0[0] = (~SW[1]&~SW[0]) | (SW[1]&~SW[0]);
+    assign HEX0[1] = (~SW[1]&SW[0]);
+    assign HEX0[2] = (~SW[1]&SW[0]);
+    assign HEX0[3] = (SW[1]&~SW[0]);
+    assign HEX0[4] = (SW[1]&~SW[0]);
+    assign HEX0[5] = (SW[1]&~SW[0]) | (~SW[1]&~SW[0]);
+    assign HEX0[6] = (SW[1]&~SW[0]) | (SW[1]&SW[0]);
 endmodule
